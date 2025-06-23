6179d8955fb07641b4d3bb9014f08f76964b4e74
tangwang <10799359+kingt527@user.noreply.gitee.com>
2025-06-23 09:09:13 +0800

    peizhi commit

--- mall-gateway/src/main/java/com/macro/mall/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/macro/mall/MallGatewayApplication.java
@@ -3,6 +3,11 @@ package com.macro.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,5 +16,4 @@ public class MallGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallGatewayApplication.class, args);
     }
-
 }
